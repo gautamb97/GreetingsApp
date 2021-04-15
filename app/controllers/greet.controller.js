487008ec502e3c01a797430c5cc56052abd89b58
gautamb97 <www.gautam971997@gmail.com>
2021-04-15 18:27:41 +0530

    refactor: Written Controller Functions

---
+++ app/controllers/greet.controller.js
@@ -0,0 +1,26 @@
+const Greet = require('../models/greet.models.js');
+
+// Create and Save a new Greet
+exports.create = (req, res) => {
+
+};
+
+// Retrieve and return all greets from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single greet with a greetId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a greet identified by the greetId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a greet with the specified greetId in the request
+exports.delete = (req, res) => {
+
+};
