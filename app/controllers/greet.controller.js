f1c4afc731a81130569addd81ab1c7ae2966b84a
gautamb97 <www.gautam971997@gmail.com>
2021-04-15 18:30:01 +0530

    refactor: Create A Note

--- app/controllers/greet.controller.js
+++ app/controllers/greet.controller.js
@@ -2,7 +2,28 @@ const Greet = require('../models/greet.models.js');
 
 // Create and Save a new Greet
 exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Greet content can not be empty"
+        });
+    }
 
+    // Create a Greet
+    const note = new Greet({
+        title: req.body.title || "Untitled Greet", 
+        content: req.body.content
+    });
+
+    // Save Greet in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Greet."
+        });
+    });
 };
 
 // Retrieve and return all greets from the database.
